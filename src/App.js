39d7ce42a8e44c9f2d8f655cac20b12d85ad1d19
Kelly Nelson <kelnel86@gmail.com>
2022-10-27 11:55:12 -0500

    creates components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <div id="image-container">
+            <img src="https://imgur.com/FRqvAlJ.png" alt="headshot"/>
+        </div>
+        <h2>About Kelly</h2>
+    </section>
+  )
+}
+
+export default About
