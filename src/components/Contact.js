cfb0b65072f97d8f30903964383f3e1780775017
Kelly Nelson <kelnel86@gmail.com>
2022-10-27 14:30:38 -0500

    adds style variables and contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import CV from "../assets/resume/resume.pdf"
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
+import {MdOutlineMail} from 'react-icons/md'
+import {GrDocumentText} from 'react-icons/gr'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="contact"> 
+      <h1>Let's get in touch.</h1>
+      <div id="icon-div">
+        <a href="https://github.com/kellynelson13" target="_blank"><BsGithub />  kellynelson13</a>
+        <a
+          href="https://www.linkedin.com/in/kelly-nelson-developer/
+"
+          target="_blank"
+        ><BsLinkedin/>  kelly-nelson-developer</a>
+        <a href="" target="_blank"><MdOutlineMail/>  kelnel86@gmail.com</a>
+        <a href={CV} download><GrDocumentText/>  Resume</a>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
