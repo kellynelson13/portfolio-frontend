c305df8c917a67c164509401dc41b2984a94ae8c
Kelly Nelson <kelnel86@gmail.com>
2022-10-27 14:45:30 -0500

    adds skills content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,50 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section id="skills">
+        <div>
+            <h5>Front End</h5>
+            <ul>
+                <li>JavaScript</li>
+                <li>React.js</li>
+                <li>CSS / Sass</li>
+                <li>HTML</li>
+                <li>jQuery</li>
+                <li>Material-UI</li>
+                <li>React-Bootstrap</li>
+                <li>EJS</li>
+                <li>GitHub</li>
+            </ul>
+        </div>
+        <div>
+            <h5>Back End</h5>
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                <li>Python</li>
+                <li>Django</li>
+                <li>PostgreSQL</li>
+                <li>Heroku</li>
+            </ul>
+        </div>
+        <div>
+            <h5>Soft Skills</h5>
+            <ul>
+                <li>Empathy</li>
+                <li>Communication</li>
+                <li>Teamwork</li>
+                <li>Problem-Solving</li>
+                <li>Perseverance</li>
+                <li>Attention to Detail</li>
+                <li>Critical Thinking</li>
+                <li>Organizational</li>
+            </ul>
+        </div>
+
+        
+    </section>
   )
 }
 
