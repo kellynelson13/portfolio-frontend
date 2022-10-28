8c24b90f9bb2dc8f66ca8dbace1e5615dd980134
Kelly Nelson <kelnel86@gmail.com>
2022-10-28 14:31:44 -0500

    usestate for nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineHome} from 'react-icons/ai'
 import {BsFilePerson} from 'react-icons/bs'
 import {AiOutlineUnorderedList} from 'react-icons/ai'
@@ -6,14 +6,17 @@ import {GrProjects} from 'react-icons/gr'
 import {BiMessageAltDetail} from 'react-icons/bi'
 
 
+
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState("#home");
+
   return (
     <nav>
-        <a class="active" href="#home"><AiOutlineHome /></a>
-        <a href="#about"><BsFilePerson /></a>
-        <a href="#skills"><AiOutlineUnorderedList /></a>
-        <a href="#projects"><GrProjects /></a>
-        <a href="#contact"><BiMessageAltDetail /></a>
+        <a class={activeNav === "#home" ? "active" : ''} onClick={() => setActiveNav("#home")} href="#home"><AiOutlineHome /></a>
+        <a class={activeNav === "#about" ? "active" : ''} onClick={() => setActiveNav("#about")} href="#about" ><BsFilePerson /></a>
+        <a class={activeNav === "#skills" ? "active" : ''} onClick={() => setActiveNav("#skills")} href="#skills"><AiOutlineUnorderedList /></a>
+        <a class={activeNav === "#projects" ? "active" : ''} onClick={() => setActiveNav("#projects")} href="#projects"><GrProjects /></a>
+        <a class={activeNav === "#contact" ? "active" : ''} onClick={() => setActiveNav("#contact")} href="#contact"><BiMessageAltDetail /></a>
     </nav>
   )
 }
