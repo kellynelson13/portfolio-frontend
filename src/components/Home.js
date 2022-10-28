39b2b2dffe8de123a1aa715d6c35ddd4130b8d09
Kelly Nelson <kelnel86@gmail.com>
2022-10-28 15:52:22 -0500

    home animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <section id="home">
-        <h3>Hello! I am</h3>
-        <h1>Kelly Nelson</h1>
-        <h3>a Full Stack Developer</h3>
+        <h3 className="h3 animate__animated animate__backInRight animate__delay-0.5s">Hello! I am</h3>
+        <h1 class="name animate__animated animate__backInLeft animate__delay-0.5s">Kelly Nelson</h1>
+        <h3 className=" animate__animated animate__backInRight animate__delay-0.5s h3">a Full Stack Developer</h3>
+        
     </section>
   )
 }
