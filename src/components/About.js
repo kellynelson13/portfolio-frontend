ae24fc6c07355a605474500fe1be4e714ac86a46
Kelly Nelson <kelnel86@gmail.com>
2022-10-28 16:19:50 -0500

    styles about page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <img src="https://imgur.com/FRqvAlJ.png" alt="headshot"/>
         </div>
         <div id="about-text">
-            <h2>I'm a full stack developer with a passion for writing clean, elegant, and thoughtful code.</h2>
+            <h2 className="h2">I'm a full stack developer with a passion for writing clean, elegant, and thoughtful code.</h2>
             <p>I have gradueted from a Bootcamp through General Assembly where I learned how to code using HTML, CSS, JavaScript, React, Express, EJS, Mongoose, and Python. Before this, I did about 6 months of self-study, including Codecademy's HTML, CSS, JavaScript, Intermediate JavaScript, and React courses. I definitely went down some deep Youtube rabbit holes getting excited about React and all things front-end! As you can imagine, I am learning something new everyday and am excited to continue this journey. </p>
             <h4>Prior to entering the tech world . . .</h4>
             <p>I spent 11 years as a nutritionist where I was the head nutrition practitioner, clinic manager, and lead instructor at my company. Several years into this, I built my own practice where I saw 4-5 clients a day while continuing to run things at the clinic I still worked at. All of this experience primed me to be an excellent software engineer as it taught me how to manage and prioritize my many daily tasks while actively assessing my clients' health needs and quickly coming up with action steps.</p>
