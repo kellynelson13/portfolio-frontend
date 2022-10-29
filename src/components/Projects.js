4b00ef571656dc2d2860a0bc138558a8d5b56a3a
Kelly Nelson <kelnel86@gmail.com>
2022-10-29 13:47:42 -0500

    better card styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,7 @@ import Row from 'react-bootstrap/Row';
 
 
 
+
 const Projects = () => {
 
     const [projects, setProjects] = useState(null)
@@ -36,20 +37,17 @@ const Projects = () => {
             {projects.map(proj => {
             return (
                 <Col>
-                <Card style={{ width: '30rem', marginLeft: "70px"}}>
+                <Card style={{ width: '30rem', marginLeft: "70px", boxShadow: "0 5px 9px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}}>
                     <Card.Img variant="top" src={proj.image} />
-                    <Card.Body style={{ backgroundColor: "var(--cloud)"}}>
+                    <Card.Body style={{ backgroundColor: "white"}}>
                         <Card.Title>{proj.name}</Card.Title>
                         <Card.Text>{proj.description}</Card.Text>
+                        <Card.Text>Main technologies used: <span>{proj.skills[0]}, {proj.skills[1]}, {proj.skills[2]}</span></Card.Text>
                     </Card.Body>
-                    <ListGroup  className="list-group-flush">
-                        <ListGroup.Item style={{ backgroundColor: "var(--mint)" }}>{proj.skills[0]}</ListGroup.Item>
-                        <ListGroup.Item style={{ backgroundColor: "var(--pink)" }}>{proj.skills[1]}</ListGroup.Item>
-                        <ListGroup.Item style={{ backgroundColor: "var(--mint)" }}>{proj.skills[2]}</ListGroup.Item>
-                    </ListGroup>
-                    <Card.Body style={{ backgroundColor: "var(--cloud)" }}>
-                        <Button style={{ backgroundColor: "var(--teal)"}} className="button" href={proj.git}>GitHub Repo</Button>
-                        <Button style={{ backgroundColor: "var(--teal)"}} className="button m-3" href={proj.live}>Live Demo</Button>
+                    
+                    <Card.Body style={{ backgroundColor: "white" }}>
+                        <Button style={{ backgroundColor: "white", color: "var(--teal)",  border: "2px solid var(--teal)"}} className="button" href={proj.git}>GitHub Repo</Button>
+                        <Button style={{ backgroundColor: "white", color: "var(--teal)",  border: "2px solid var(--teal)"}} className="button m-3" href={proj.live}>Live Demo</Button>
                     </Card.Body>
                 </Card>
                 </Col>
