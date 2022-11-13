2b27820dc6165c29175109c0e0a81c8ca95dcafb
Kelly Nelson <kelnel86@gmail.com>
2022-11-13 15:53:06 -0600

    change tag line on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <section id="home">
         <h3 className="h3 animate__animated animate__backInRight animate__delay-0.5s">Hello! I am</h3>
         <h1 class="name animate__animated animate__backInLeft animate__delay-0.5s">Kelly Nelson</h1>
-        <h3 className=" animate__animated animate__backInRight animate__delay-0.5s h3">a Full Stack Developer</h3>
+        <h3 className=" animate__animated animate__backInRight animate__delay-0.5s h3">Nutritionist turned Developer</h3>
         
     </section>
   )
